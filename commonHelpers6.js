4ea46d7ab738cf650b2cd8cc327d412f4938436a
Ostik1220 <117195748+Ostik1220@users.noreply.github.com>
2024-09-07 14:18:47 +0000

    Deploying to gh-pages from @ Tate-T/CBD-event-project@e599feafbbc1f7707325567849f6cc26d2536b17 🚀

---
+++ commonHelpers6.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";var s=document.getElementsByClassName("choice__button");for(var e=0;e<s.length;e++)s[e].addEventListener("click",function(){this.classList.toggle("actives");var a=this.getAttribute("data-target");this.classList.toggle("active");var a=this.getAttribute("data-target"),t=document.getElementById(a);t.style.display==="block"?t.style.display="none":t.style.display="block"});
+//# sourceMappingURL=commonHelpers6.js.map
